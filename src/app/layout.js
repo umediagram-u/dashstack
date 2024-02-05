50307b9a43cca92a8c2ce13707eda97aca7f0942
Usmonjon Hasanov <usmonjonhasanov777@gmail.com>
2024-02-05 16:43:42 +0500

    Bugs ficed

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,7 +15,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <script src="https://cdn.tailwindcss.com"></script>
         <div id="root">{children}</div>
       </body>
     </html>
