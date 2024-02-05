580ff5b7d12ca7c673e0499ab441883c1a60fd75
Usmonjon Hasanov <usmonjonhasanov777@gmail.com>
2024-02-05 16:37:09 +0500

    Bugs fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Nunito_Sans } from "next/font/google";
 import "./globals.css";
+import { Head } from "next/document";
 
 const inter = Nunito_Sans({
   subsets: ["latin"],
@@ -15,6 +16,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <script src="https://cdn.tailwindcss.com"></script>
         <div id="root">{children}</div>
       </body>
     </html>
